62d2963525b7ff450626a29b98b9f43da601f60c
Gaurav Kumar <gaurav@m4.mynetworksettings.com>
2025-05-21 13:53:21 -0400

    chore: ensure .env.local is gitignored and update examples/pcu.test.ts

---
+++ examples/pcu.test.ts
@@ -0,0 +1,7 @@
+import { shortest } from "@antiwork/shortest";
+
+shortest("login with company id PSD, username support, password H0tl@nt@");
+shortest("go to orders menu then click Quick search, Enter glove in key word, click search key words");
+shortest("increase the quantity of the first item to 1000, click add to cart");
+shortest("go to cart, click checkout");
+shortest("go to checkout, click continue");
